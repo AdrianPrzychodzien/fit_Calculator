cd3dd0c75bd11199be361cb6b16a322d9076b912
Adrian Przychodzień <adrianprzychodzien@gmail.com>
2020-01-12 17:47:48 +0100

    FormInput styles rewrite into scss, user personal data form basic structure

--- src/pages/personalData.js
+++ src/pages/personalData.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { withStyles } from '@material-ui/core'
 
-import FormInput from '../components/FormInput'
+import FormInput from '../util/FormInput/FormInput'
 import CustomButton from '../util/CustomButton/CustomButton'
 
 import HeightIcon from '@material-ui/icons/Height';
+import { Rating } from '@material-ui/lab'
 
 const styles = theme => ({
   personalData: {
@@ -14,14 +15,16 @@ const styles = theme => ({
 
 const PersonalData = ({ classes }) => {
   const [userData, setUserData] = useState({
-    height: '',
+    height: '0',
     weight: '',
     age: '',
     sex: '',
-    lifeActivity: ''
+    lifeActivity: 1
   })
 
   const { height, weight, age, sex, lifeActivity } = userData
+
+  console.log(userData);
 
   const handleChange = e => {
     const { name, value } = e.target
@@ -61,22 +64,40 @@ const PersonalData = ({ classes }) => {
           label='age'
           required
         />
-        <FormInput
-          type='radio'
-          name='sex'
-          value={sex}
-          onChange={handleChange}
-          label='sex'
-          required
-        />
-        <FormInput
-          type='text'
-          name='lifeActivity'
-          value={lifeActivity}
-          onChange={handleChange}
-          label='Life Activity'
-          required
-        />
+        <h4>Sex:
+          <label>
+            <input
+              type='radio'
+              name='sex'
+              value='Male'
+              checked={sex === 'Male'}
+              onChange={handleChange}
+              required
+            />
+            Male
+          </label>
+          <label>
+            <input
+              type='radio'
+              name='sex'
+              value='Female'
+              checked={sex === 'Female'}
+              onChange={handleChange}
+              required
+            />
+            Female
+            </label>
+        </h4>
+        <h4>Life activity:
+        <Rating
+            name="life activity"
+            value={lifeActivity}
+            onChange={(event, newValue) => {
+              setUserData({ ...userData, lifeActivity: newValue })
+            }}
+            required
+          />
+        </h4>
         <CustomButton type='submit'>ADD DATA</CustomButton>
       </form>
     </div>

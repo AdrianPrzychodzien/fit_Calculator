ac96720eacce2bb74f2aac1210411abaf1fba720
Adrian Przychodzień <adrianprzychodzien@gmail.com>
2020-01-19 21:18:58 +0100

    selectItem function to show content basing on clicked element

--- src/pages/calories/Calories.js
+++ src/pages/calories/Calories.js
@@ -12,6 +12,30 @@ import './Calories.scss'
 const Calories = ({ userData, history }) => {
   const { height, weight, age, sex, lifeActivity, fat } = userData
 
+  const tabItems = document.querySelectorAll('.tab--item')
+  const tabContentItems = document.querySelectorAll('.tabContent--item')
+
+  // Select tab content item
+  function selectItem(e) {
+    removeBorder()
+    removeShow()
+    // Add border to current tab
+    this.classList.add('tab--border')
+    // Grab content item from DOM
+    const tabContentItem = document.querySelector(`#${this.id}--content`)
+    // Add show class
+    tabContentItem.classList.add('show')
+  }
+
+  const removeBorder = () => {
+    tabItems.forEach(item => item.classList.remove('tab--border'))
+  }
+
+  const removeShow = () => {
+    tabContentItems.forEach(item => item.classList.remove('show'))
+  }
+
+  tabItems.forEach(item => item.addEventListener('click', selectItem))
 
   if (height && weight && age && sex && lifeActivity && fat) {
     return (
@@ -22,8 +46,54 @@ const Calories = ({ userData, history }) => {
         <hr />
         <h4 className="calories__text">
           Calories intake on a different activity level
-          <ActivityCaloriesInfo />
+          <div className="calories__icon">
+            <ActivityCaloriesInfo />
+          </div>
         </h4>
+        <hr />
+
+        <h2 className="calories__title">Macronutrients</h2>
+        <div className="tabs">
+          <div className="container">
+            <div id="tab--1" className="tab--item tab--border">
+              Maintenance
+            </div>
+            <div id="tab--2" className="tab--item">
+              Cutting
+            </div>
+            <div id="tab--3" className="tab--item">
+              Bulking
+            </div>
+          </div>
+        </div>
+        <div className="tabContent">
+          <div className="tabContent__container">
+            {/* Tab Content 1 */}
+            <div id="tab--1--content" className="tabContent--item show">
+              <h5>These macronutrient values reflect your
+              maintenance calories of ... calories per day.</h5>
+              ...table1
+            </div>
+
+            {/* Tab Content 2 */}
+            <div id="tab--2--content" className="tabContent--item">
+              <h5>These macronutrient values reflect your
+                cutting calories of ... calories per day,
+                which is a 500 calorie per day deficit from
+              your maintenance of ... calories per day.</h5>
+              ...table2
+            </div>
+
+            {/* Tab Content 3 */}
+            <div id="tab--3--content" className="tabContent--item">
+              <h5>These macronutrient values reflect your
+                bulking calories of ... calories per day,
+                 which is +500 calories per day from
+               your maintenance of ... calories per day.</h5>
+              ...table3
+            </div>
+          </div>
+        </div>
       </div>
     )
   } else

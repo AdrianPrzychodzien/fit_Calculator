ee4c88bc39ce34f9046990f9d2956c1880134cde
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2020-01-20 20:28:06 +0100

    functions to calculate macronutrients amount on different diets, added table with distribution of macronutrients on different diets

--- src/pages/calories/Calories.js
+++ src/pages/calories/Calories.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 
 import { setFatData, setFatPercentage } from '../../redux/actions'
-import { MifflinStJeor } from '../../util/equations'
+import { MifflinStJeor, ModerateCarb, LowCarb, HighCarb } from '../../util/equations'
 
 import ActivityCaloriesInfo from '../../components/Info/ActivityCaloriesInfo/ActivityCaloriesInfo'
 import CustomButton from '../../util/CustomButton/CustomButton'
@@ -10,11 +10,24 @@ import CustomButton from '../../util/CustomButton/CustomButton'
 import './Calories.scss'
 
 const Calories = ({ userData, history }) => {
+  const [data, setData] = useState({
+    diet: 'Maintenance'
+  })
+
   const { height, weight, age, sex, lifeActivity, fat } = userData
+
+  const kcalAmount = data.diet === 'Maintenance' ?
+    MifflinStJeor(userData) : data.diet === 'Cutting' ?
+      MifflinStJeor(userData) - 500 : MifflinStJeor(userData) + 500
+
+  const [proteinModerate, carbsModerate, fatsModerate] = ModerateCarb(kcalAmount)
+  const [proteinLow, carbsLow, fatsLow] = LowCarb(kcalAmount)
+  const [proteinHigh, carbsHigh, fatsHigh] = HighCarb(kcalAmount)
 
   const tabItems = document.querySelectorAll('.tab--item')
   const tabContentItems = document.querySelectorAll('.tabContent--item')
 
+  tabItems.forEach(item => item.addEventListener('click', selectItem))
   // Select tab content item
   function selectItem(e) {
     removeBorder()
@@ -27,22 +40,20 @@ const Calories = ({ userData, history }) => {
     tabContentItem.classList.add('show')
   }
 
-  const removeBorder = () => {
+  function removeBorder() {
     tabItems.forEach(item => item.classList.remove('tab--border'))
   }
 
-  const removeShow = () => {
+  function removeShow() {
     tabContentItems.forEach(item => item.classList.remove('show'))
   }
-
-  tabItems.forEach(item => item.addEventListener('click', selectItem))
 
   if (height && weight && age && sex && lifeActivity && fat) {
     return (
       <div className="calories">
         <h2 className="calories__title">Caloric needs: {MifflinStJeor(userData)} kcal</h2>
         <h4>Based on your stats, the best estimate for your maintenance calories
-           is 3,577 calories per day based on the Mifflin-St Jeor Formula</h4>
+           is {MifflinStJeor(userData)} calories per day based on the Mifflin-St Jeor Formula</h4>
         <hr />
         <h4 className="calories__text">
           Calories intake on a different activity level
@@ -55,42 +66,141 @@ const Calories = ({ userData, history }) => {
         <h2 className="calories__title">Macronutrients</h2>
         <div className="tabs">
           <div className="container">
-            <div id="tab--1" className="tab--item tab--border">
+            <div onClick={() => setData({ diet: 'Maintenance' })}
+              id="tab--1"
+              className="tab--item tab--border"
+            >
               Maintenance
             </div>
-            <div id="tab--2" className="tab--item">
+            <div onClick={() => setData({ diet: 'Cutting' })}
+              id="tab--2"
+              className="tab--item"
+            >
               Cutting
             </div>
-            <div id="tab--3" className="tab--item">
+            <div onClick={() => setData({ diet: 'Bulking' })}
+              id="tab--3"
+              className="tab--item"
+            >
               Bulking
             </div>
           </div>
         </div>
         <div className="tabContent">
           <div className="tabContent__container">
-            {/* Tab Content 1 */}
+            {/* Tab Content 1 - Maintenance */}
             <div id="tab--1--content" className="tabContent--item show">
               <h5>These macronutrient values reflect your
-              maintenance calories of ... calories per day.</h5>
-              ...table1
+              maintenance calories of {MifflinStJeor(userData)} kcal per day.</h5>
+
+              <table className="tabContent__table">
+                <thead>
+                  <tr>
+                    <th>Macro</th>
+                    <th>Moderate Carb</th>
+                    <th>Low Carb</th>
+                    <th>High Carb</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Protein</td>
+                    <td>{proteinModerate}g</td>
+                    <td>{proteinLow}g</td>
+                    <td>{proteinHigh}g</td>
+                  </tr>
+                  <tr >
+                    <td>Carbs</td>
+                    <td>{carbsModerate}g</td>
+                    <td>{carbsLow}g</td>
+                    <td>{carbsHigh}g</td>
+                  </tr>
+                  <tr >
+                    <td>Fats</td>
+                    <td>{fatsModerate}g</td>
+                    <td>{fatsLow}g</td>
+                    <td>{fatsHigh}g</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
 
-            {/* Tab Content 2 */}
+            {/* Tab Content 2 - Cutting */}
             <div id="tab--2--content" className="tabContent--item">
               <h5>These macronutrient values reflect your
-                cutting calories of ... calories per day,
-                which is a 500 calorie per day deficit from
-              your maintenance of ... calories per day.</h5>
-              ...table2
+                cutting calories of {MifflinStJeor(userData) - 500} kcal per day,
+                which is a 500 calorie deficit from
+              your maintenance of {MifflinStJeor(userData)} calories.</h5>
+
+              <table className="tabContent__table">
+                <thead>
+                  <tr>
+                    <th>Macro</th>
+                    <th>Moderate Carb</th>
+                    <th>Low Carb</th>
+                    <th>High Carb</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Protein</td>
+                    <td>{proteinModerate}g</td>
+                    <td>{proteinLow}g</td>
+                    <td>{proteinHigh}g</td>
+                  </tr>
+                  <tr >
+                    <td>Carbs</td>
+                    <td>{carbsModerate}g</td>
+                    <td>{carbsLow}g</td>
+                    <td>{carbsHigh}g</td>
+                  </tr>
+                  <tr >
+                    <td>Fats</td>
+                    <td>{fatsModerate}g</td>
+                    <td>{fatsLow}g</td>
+                    <td>{fatsHigh}g</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
 
-            {/* Tab Content 3 */}
+            {/* Tab Content 3 - Bulking */}
             <div id="tab--3--content" className="tabContent--item">
               <h5>These macronutrient values reflect your
-                bulking calories of ... calories per day,
-                 which is +500 calories per day from
-               your maintenance of ... calories per day.</h5>
-              ...table3
+                bulking calories of {MifflinStJeor(userData) + 500} kcal per day,
+                which is +500 calories from
+               your maintenance of {MifflinStJeor(userData)} calories.</h5>
+
+              <table className="tabContent__table">
+                <thead>
+                  <tr>
+                    <th>Macro</th>
+                    <th>Moderate Carb</th>
+                    <th>Low Carb</th>
+                    <th>High Carb</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Protein</td>
+                    <td>{proteinModerate}g</td>
+                    <td>{proteinLow}g</td>
+                    <td>{proteinHigh}g</td>
+                  </tr>
+                  <tr >
+                    <td>Carbs</td>
+                    <td>{carbsModerate}g</td>
+                    <td>{carbsLow}g</td>
+                    <td>{carbsHigh}g</td>
+                  </tr>
+                  <tr >
+                    <td>Fats</td>
+                    <td>{fatsModerate}g</td>
+                    <td>{fatsLow}g</td>
+                    <td>{fatsHigh}g</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
